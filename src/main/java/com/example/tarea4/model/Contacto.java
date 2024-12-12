8fdfe722d2a66a66e09fd660321cac8b8c410bc5
fabian-castro-c <fabian.castro.c@ug.uchile.cl>
2024-12-12 02:46:01 -0300

    Correccion en el formato de los id

--- src/main/java/com/example/tarea4/model/Contacto.java
+++ src/main/java/com/example/tarea4/model/Contacto.java
@@ -10,7 +10,7 @@ public class Contacto {
     // Id del contacto
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     // Nombre del contacto
     @Column(name = "nombre", length = 80, nullable = false)
@@ -30,11 +30,11 @@ public class Contacto {
 
     // Getters and Setters
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 

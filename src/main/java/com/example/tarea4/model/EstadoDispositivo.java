5f8440e0794c71643917310f9fc688c2b990ba96
fabian-castro-c <fabian.castro.c@ug.uchile.cl>
2024-12-12 01:12:24 -0300

    Enums de EstadoDispositivo y enums de TipoDispositivo.

---
+++ src/main/java/com/example/tarea4/model/EstadoDispositivo.java
@@ -0,0 +1,7 @@
+package com.example.tarea4.model;
+
+public enum EstadoDispositivo {
+    FUNCIONA_PERFECTO,
+    FUNCIONA_A_MEDIAS,
+    NO_FUNCIONA
+}
---
+++ src/main/java/com/example/tarea4/model/TipoDispositivo.java
@@ -0,0 +1,15 @@
+package com.example.tarea4.model;
+
+public enum TipoDispositivo {
+    PANTALLA,
+    NOTEBOOK,
+    TABLET,
+    CELULAR,
+    CONSOLA,
+    MOUSE,
+    TECLADO,
+    IMPRESORA,
+    PARLANTE,
+    AUDIFONOS,
+    OTRO
+}

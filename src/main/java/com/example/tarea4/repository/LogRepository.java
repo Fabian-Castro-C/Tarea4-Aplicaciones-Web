5259886fb1bd42b5d2d7ebe0bc2f79d323f0e6cc
fabian-castro-c <fabian.castro.c@ug.uchile.cl>
2024-12-14 16:44:48 -0300

    Repository de Log

---
+++ src/main/java/com/example/tarea4/repository/LogRepository.java
@@ -0,0 +1,9 @@
+package com.example.tarea4.repository;
+
+import com.example.tarea4.model.Log;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LogRepository extends JpaRepository<Log, Long> {
+}
